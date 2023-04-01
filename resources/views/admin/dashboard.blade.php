f9ee6d363b726b067ae0aea660518c71684455ec
lhawi <lukmanhawi@gmail.com>
2023-04-01 18:11:04 +0700

    Integration admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -36,10 +36,12 @@
                         @endif
                     </td>
                     <td>
-                        <form action="">
-                            @csrf
-                            <button class="btn btn-primary btn-sm">Set to paid</button>
-                        </form>
+                        @if (!$checkout->is_paid)
+                            <form action="{{route('admin.checkout.update', $checkout->id)}}" method="post">
+                                @csrf
+                                <button class="btn btn-primary btn-sm">Set to Paid</button>
+                            </form>
+                        @endif
                     </td>
                    </tr> 
                 @empty
